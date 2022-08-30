88cef08822ccc819f5329fd6257e73ef7ab6b4fc
AnitaAO <anitaisbeta@gmail.com>
2022-08-30 11:39:40 +0100

    2 files modified

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -11,27 +11,26 @@ export class PostsComponent implements OnInit {
   private url = 'https://jsonplaceholder.typicode.com/posts';
 
   constructor(private httpClient: HttpClient) { 
-    this.httpClient.get(this.url).subscribe(response: any[] => {
-      this.posts = response;
+    this.httpClient.get(this.url)
+      .subscribe((response: any[]) => {
+        this.posts = response;
     });
-
-    // createPost(input: HTMLInputElement) {
-    //   let post = {input: input.value};
-    //   input.value = "";
-      
-    //   // this.httpClient.post(this.url, post)
-    //   //   .subscribe(response => {
-    //   //     post['id'] = response.id;
-    //   //     this.posts.splice(0, 0, post);
-    //   //   });
-    // }
-
-    
   }
 
-
-
+  
   ngOnInit() {
   }
+  
+  createPost(input: HTMLInputElement) {
+    let post = {input: input.value};
+    input.value = "";
+    
+    // this.httpClient.post(this.url, post)
+    //   .subscribe(response => {
+    //     post['id'] = response.id;
+    //     this.posts.splice(0, 0, post);
+    //   });
+  }
+
 
 }
